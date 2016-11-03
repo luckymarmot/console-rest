97fcf39b47bdd6304dde1dd745b4be04e0e08e35
JonathanMontane <jon.montane@gmail.com>
2016-11-03 11:30:59 +0100

    missing helper statement

--- src/components/helpers/MissingHelper.js
+++ src/components/helpers/MissingHelper.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import GithubLogo from 'crest/basics/media/logos/GithubLogo'
+
 require('./helper-content.styl')
 
 export default class MissingHelper extends Component {
@@ -18,8 +20,16 @@ export default class MissingHelper extends Component {
 
         /* eslint-disable max-len */
         return <div className={classes}>
-            <div className="console-support">
-                Missing Statement
+            <div className="missing-support">
+                Console.REST aims at being as universal as possible.
+                If your favorite format or client is missing, you can let us
+                know here:
+                <a className="action-row"
+                    href="https://github.com/luckymarmot/console-rest/issues/12"
+                    target="_blank">
+                    <GithubLogo/>
+                    Github Issue
+                </a>
             </div>
         </div>
         /* eslint-enable max-len */
